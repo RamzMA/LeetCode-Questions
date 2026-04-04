86ed8d8715ff4e4d3aea06acc05c359fd3a791b0
Ramz <ramzahmedm@gmail.com>
2026-04-04 17:11:40 +0100

    Updated brute force for 2dmatrix

--- BinarySearch/SearchA2DMatrix.py
+++ BinarySearch/SearchA2DMatrix.py
@@ -1,7 +1,14 @@
+from typing import List
+
+
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
+            for c in range(len(matrix)):
                 if matrix[r][c] == target:
                     return True
         return False
+
+
+solution = Solution()
+print(solution.searchMatrix([[1, 2, 4, 8], [10, 11, 12, 13], [14, 20, 30, 40]], 10))
